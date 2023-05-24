41b25b1b5242b7ab00fea3b2d6990344e46c07e6
Phil Ecker <hello@philecker.com>
2023-05-24 18:21:41 -0500

    Fixed issue with right column repo star overflow

--- firefox/vertifix.js
+++ firefox/vertifix.js
@@ -1,3 +1,10 @@
 (() => {
-  document.body.getElementsByTagName("main")[0].parentElement.style.width = "100%"
+  document.body.getElementsByTagName("main")[0].parentElement.style.width = "100%";
+  const rightColumn = document.body.getElementsByClassName("team-left-column")[1];
+  const elements = rightColumn.getElementsByClassName("Truncate-text");
+
+  Array.from(elements).forEach((el) => {
+    el.style.minWidth = "initial";
+    el.style.overflow = "visible";
+  });
 })();
